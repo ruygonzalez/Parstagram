d038968317c8cda189b78195ee8083e53e78716c
Ruy Gonzalez Hermosillo <ruygonzalez@fb.com>
2018-07-09 15:54:55 -0700

    Initial commit

---
+++ app/src/main/java/me/ruygonzalez/parstagram/MainActivity.java
@@ -0,0 +1,55 @@
+package me.ruygonzalez.parstagram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_et);
+        passwordInput = findViewById(R.id.password_et);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get references to username and password that user typed once user clicks on Log in button
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+                login(username, password);
+            }
+        });
+    }
+
+    private void login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) { // if no errors, so user was logged in correctly
+                    Log.d("LoginActivity", "Login successful!");
+                }
+                else { // if there is a ParseException
+                    Log.e("LoginActivity", "Login failure.");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
